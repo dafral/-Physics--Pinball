09cbf002999f2c4ecdc04908337afe418681d471
rogerbusquets97 <duranbusquetsroger@gmail.com>
2016-10-19 17:48:40 +0200

    Pinball
    Basic code, window created, background rendered and box2d added.

---
+++ Pinball/ModuleSceneIntro.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Module.h"
+#include "p2List.h"
+#include "Globals.h"
+
+class PhysBody;
+
+class ModuleSceneIntro : public Module
+{
+public:
+	ModuleSceneIntro(Application* app, bool start_enabled = true);
+	~ModuleSceneIntro();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+
+public:
+
+	SDL_Texture* background;
+	
+};
